c005aefea177fedb291b6733e7912035a50cea71
polyorbstudios <polyorbstudios@gmail.com>
2026-09-19 23:31:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Preencha com a API Key do seu Power-Up (ver README.md, passo 3). */
 window.TH_CONFIG = {
-  appKey: 'COLE_SUA_API_KEY_AQUI',
+  appKey: 'b5a915808b270bed3718ed1ec0ba1377',
   appName: 'Controle de Horas'
 };
